3d75e60c1628cd0c554d49c03acda224bbcc01eb
Milan <milanjulinac996@gmail.com>
2020-04-25 14:49:50 +0200

    Finished login and register routes, added validation

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
     "mongodb+srv://milan:doom.1996@cluster0-46xli.mongodb.net/devconnector?retryWrites=true&w=majority",
+  secretOrKey: "secret",
 };
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/users");
 const profile = require("./routes/profile");
 const posts = require("./routes/posts");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const db = require("./config/keys").mongoURI;
 
@@ -13,7 +18,10 @@ mongoose
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
-app.use(express.json());
+app.use(passport.initialize());
+
+require("./config/passport")(passport);
+
 app.use("/api/users", users);
 // app.use("/api/posts", posts);
 // app.use("/api/profile", profile);
