b0db90f5725ef24bcf3496ed7e2113b796c832e4
Milan <milanjulinac996@gmail.com>
2020-04-26 14:07:08 +0200

    finished post routes added validation

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ require("./config/passport")(passport);
 
 app.use("/api/users", users);
 app.use("/api/profile", profile);
-// app.use("/api/posts", posts);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
